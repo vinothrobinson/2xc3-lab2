88dad46482af2c1d7d517c36f21ce34c24b0bc3a
Karl <saderk@mcmaster.ca>
2023-02-08 10:13:10 -0500

    BFS2 start

--- bfs2.py
+++ bfs2.py
@@ -1,11 +1,8 @@
 from collections import deque
 import graph
 
-def BFS(G, node1, node2):
-    lists = []
-    for i in G.adj[node1]:
-        lists.append([])
-
+def BFS2(G, node1, node2):
+    path_dict = {}
     Q = deque([node1])
     marked = {node1 : True}
     for node in G.adj:
@@ -14,9 +11,31 @@ def BFS(G, node1, node2):
     while len(Q) != 0:
         current_node = Q.popleft()
         for node in G.adj[current_node]:
-            if node == node2:
-                return True
+            #if node == node2:
+                #return True
             if not marked[node]:
+                path_dict[node] = []
+                for i in G.adj[node]:
+                    if not marked[i]:
+                        path_dict[node].append(i)
                 Q.append(node)
                 marked[node] = True
-    return False
+
+    final_list = deque([])
+    found_path = False
+    while found_path == False:
+        for list in path_dict.values():
+            if node2 in list:
+                final_list.appendleft(node2)
+                key = findKey(path_dict, node2)
+            else:
+                found_path = True
+    return []
+
+def findKey(path_dict, node):
+    pass
+
+'''
+t = ("hi","ho")
+print(t[1])
+'''
