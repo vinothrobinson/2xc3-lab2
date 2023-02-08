e7d8eac7ec5c50581a9b4edf29c1a808fdc6346a
Karl <saderk@mcmaster.ca>
2023-02-08 09:22:27 -0500

    Test commit

---
+++ bfs2.py
@@ -0,0 +1,22 @@
+from collections import deque
+import graph
+
+def BFS(G, node1, node2):
+    lists = []
+    for i in G.adj[node1]:
+        lists.append([])
+
+    Q = deque([node1])
+    marked = {node1 : True}
+    for node in G.adj:
+        if node != node1:
+            marked[node] = False
+    while len(Q) != 0:
+        current_node = Q.popleft()
+        for node in G.adj[current_node]:
+            if node == node2:
+                return True
+            if not marked[node]:
+                Q.append(node)
+                marked[node] = True
+    return False
