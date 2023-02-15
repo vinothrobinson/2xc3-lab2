d22cfb5a85c73831e106c18892614131413c80fe
Karl <saderk@mcmaster.ca>
2023-02-15 10:11:29 -0500

    minor fixes to exp1 and exp2

--- experiments.py
+++ experiments.py
@@ -18,28 +18,27 @@ def experiment1(node_num, trial_num):
         cycle_percentage.append(100 * cycle_count / trial_num)
     plot.plot(cycle_percentage)
     plot.title(f"Cycle Detection: Number of Edges vs Cycle Percentage (|V| = {node_num})")
-    plot.xlabel("Number of Nodes")
+    plot.xlabel("Number of Edges")
     plot.ylabel("Cycle Percentage (%)")
     plot.show()
 
-def experiment2(n):
+def experiment2(node_num, trial_num):
     total_list = []
-    tri_num = graph.triangle(n-1)
-    trial_num = 100
+    tri_num = graph.triangle(node_num-1)
     for num_of_edges in range(tri_num+1):
         list = []
         true_num = 0
         for i in range(trial_num):
-            g = graph.create_random_graph(n, num_of_edges)
+            g = graph.create_random_graph(node_num, num_of_edges)
             list.append(is_connected.is_connected(g))
         for bool in list:
             if bool:
                 true_num += 1
         total_list.append(true_num/trial_num*100)
     plot.plot(total_list)
-    plot.title("Number of Edges vs Connectedness")
+    plot.title("Number of Edges vs Connected Percentage")
     plot.xlabel("Number of Edges")
-    plot.ylabel("Connectedness (%)")
+    plot.ylabel("Connected Percentage (%)")
     plot.show()
 
 '''experiment1(5, 100)
@@ -48,4 +47,4 @@ experiment1(15, 100)
 experiment1(20, 100)
 experiment1(30, 100)'''
 
-experiment2(10)
+experiment2(10, 100)
