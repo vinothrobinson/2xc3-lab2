6469b890de5e3927563e2813bd164fd4d7a3ca5e
Brandon Bosman <brandonb@wightman.ca>
2023-02-15 08:49:55 -0500

    Added min. vertex cover functions

--- graph.py
+++ graph.py
@@ -61,6 +61,38 @@ def DFS(G, node1, node2):
                 S.append(node)
     return False
 
+
+#Use the methods below to determine minimum vertex covers
+
+def add_to_each(sets, element):
+    copy = sets.copy()
+    for set in copy:
+        set.append(element)
+    return copy
+
+def power_set(set):
+    if set == []:
+        return [[]]
+    return power_set(set[1:]) + add_to_each(power_set(set[1:]), set[0])
+
+def is_vertex_cover(G, C):
+    for start in G.adj:
+        for end in G.adj[start]:
+            if not(start in C or end in C):
+                return False
+    return True
+
+def MVC(G):
+    nodes = [i for i in range(G.get_size())]
+    subsets = power_set(nodes)
+    min_cover = nodes
+    for subset in subsets:
+        if is_vertex_cover(G, subset):
+            if len(subset) < len(min_cover):
+                min_cover = subset
+    return min_cover
+
+
 def create_random_graph(i, j):
     G = Graph(i)
     for _ in range(j):
