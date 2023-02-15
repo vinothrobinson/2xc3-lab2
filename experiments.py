4f2e49af42dc0dda81bad6575c9a8bdd13a32883
Brandon Bosman <brandonb@wightman.ca>
2023-02-15 09:51:06 -0500

    Finished experiment 1

--- experiments.py
+++ experiments.py
@@ -1,22 +1,29 @@
-import timeit
+import matplotlib.pyplot as plot
 import graph
-import timeit
+import has_cycle
 
 
-def experiment(node_num, trial_num):
-    average_times = []
+def experiment1(node_num, trial_num):
+    cycle_percentage = []
     max_edge_num = graph.triangle(node_num - 1)
-    for edge_num in range(max_edge_num):
-        times = 0
-        for _ in trial_num:
+    for edge_num in range(max_edge_num+1):
+        cycle_count = 0
+        for _ in range(trial_num):
             G = graph.create_random_graph(node_num, edge_num)
 
-            start = timeit.default_timer()
             # Run Experiment
-            end = timeit.default_timer()
-            times += end - start
-        average_times.append(times / trial_num)
-    return average_times
+            if has_cycle.has_cycle(G):
+                cycle_count += 1
+        cycle_percentage.append(cycle_count / trial_num)
+    plot.plot(cycle_percentage)
+    plot.title(f"Cycle Detection: Number of Edges vs Cycle Percentage (|V| = {node_num})")
+    plot.xlabel("Number of Nodes")
+    plot.ylabel("Cycle Percentage")
+    plot.show()
 
-for i in range(20):
-    print(f"Triangle {i}: {triangle(i)}")
+
+experiment1(5, 100)
+experiment1(10, 100)
+experiment1(15, 100)
+experiment1(20, 100)
+experiment1(30, 100)
